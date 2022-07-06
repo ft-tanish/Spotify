ec38292532ce88c452c034a35f75e70fed555230
Tanish <tanishchaurasia6@gmail.com>
2022-07-07 04:33:00 +0530

    DataLayer

--- src/App.js
+++ src/App.js
@@ -2,9 +2,15 @@ import React, { useEffect, useState } from "react";
 import './App.css';
 import Login from "./Login";
 import { getTokenFromUrl } from "./spotify";
+import SpotifyWebApi from "spotify-web-api-js";
+import Player from "./Player";
+import { useDataLayerValue } from "./DataLayer";
+
+const spotify = new SpotifyWebApi();
 
 function App() {
   const [token, setToken] = useState(null);
+  const [{ user }, dispatch] = useDataLayerValue();
 
   useEffect(() => {
     const hash = getTokenFromUrl();
@@ -13,21 +19,31 @@ function App() {
 
     if (_token){
       setToken(_token);
+
+      spotify.setAccessToken(_token);
+
+      spotify.getMe().then((user) => {
+        dispatch({
+          type: 'SET_USER',
+          user: user,
+        });
+      });
     }
 
     console.log("I HAVE A TOKEN", token);
   }, []);
 
+  console.log("person, user");
+
   return (
     <div className="app">
       {
         token ? (
-          <h1> I am logged in</h1>
+          <Player/>
         ) : (
           <Login/>
         )
       }
-        <Login/>
     </div>
   );
 }
