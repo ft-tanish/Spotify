2d4325b2623d859ef3e6af576b0637735a754be2
Tanish <tanishchaurasia6@gmail.com>
2022-07-09 21:55:44 +0530

    Sidebar

--- src/Footer.js
+++ src/Footer.js
@@ -1,10 +1,32 @@
 import React from 'react';
 import "./Footer.css";
+import PlayCircleOutlineIcon from "@mui/icons-material/PlayCircleOutline";
+import SkipPreviousIcon from "@mui/icons-material/SkipPrevious";
+import SkipNextIcon from "@mui/icons-material/SkipNext";
+import ShuffleIcon from "@mui/icons-material/Shuffle";
+import RepeatIcon from "@mui/icons-material/Repeat";
+// import VolumeDownIcon from "@mui/icons-material/VolumeDown";
+// import PauseCircleOutlineIcon from "@mui/icons-material/PauseCircleOutline";
+// import PlaylistPlayIcon from "@mui/icons-material/PlaylistPlay";
 
 function Footer() {
   return (
     <div className='footer'>
-        <h1>Footer</h1>
+        <div className='footer__left'>
+            <p>Album and song details</p>
+        </div>
+
+        <div className='footer__center'>
+            <ShuffleIcon className="footer__green"/>
+            <SkipPreviousIcon className="footer__icon"/>
+            <PlayCircleOutlineIcon fontSize="large" className="footericon"/>
+            <SkipNextIcon className="footer__icon"/>
+            <RepeatIcon className="footer__green"/>
+        </div>
+
+        <div className='footer__right'>
+            <p>Volume controls</p>
+        </div>
     </div>
   )
 }
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import "./Sidebar.css";
 import SidebarOption from './SidebarOption';
+import HomeRoundedIcon from '@mui/icons-material/HomeRounded';
+import SearchRoundedIcon from '@mui/icons-material/SearchRounded';
+import LibraryMusicIcon from '@mui/icons-material/LibraryMusic';
+import { useDataLayerValue } from "./DataLayer";
+
 
 function Sidebar() {
+    const [{ playlists }, dispatch] = useDataLayerValue();
   return (
     <div className='sidebar'>
         <img
@@ -10,10 +16,17 @@ function Sidebar() {
         src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
         alt=""
       />
-      <SidebarOption title="Home" />
-      <SidebarOption title="Search"/>
-      <SidebarOption title="Your Library"/>
-
+      <SidebarOption Icon={HomeRoundedIcon} title="Home" />
+      <SidebarOption Icon={SearchRoundedIcon} title="Search"/>
+      <SidebarOption Icon={LibraryMusicIcon} title="Your Library"/>
+      <strong className='sidebar__title'>PLAYLISTS</strong>
+      <hr/>
+      
+      {playlists?.items?.map(playlist => (
+      <SidebarOption title={playlist.name}/>
+      )
+      )}
+      
         </div>
   )
 }
